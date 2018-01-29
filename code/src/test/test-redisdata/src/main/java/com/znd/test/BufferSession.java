a539a6c82cb3b3a4de327eacaa38e26e91696bf9
hilockman <43579183@qq.com>
2018-01-29 17:26:33 +0800

    update

--- code/src/test/test-redisdata/src/main/java/com/znd/test/BufferSession.java
+++ code/src/test/test-redisdata/src/main/java/com/znd/test/BufferSession.java
@@ -1,8 +1,24 @@
 package com.znd.test;
 
 import java.io.Closeable;
+import java.util.List;
+import java.util.Map;
 
 public interface BufferSession  extends Closeable {
 
+	
+	<T> T selectOne(String statement, Object parameter);
+	<E> List<E> selectList(String statement, Object parameter);
+	<K,V> Map<K,V> selectMap(String statement, Object parameter, String mapKey);
+	int insert(String statement, Object parameter);
+	int update(String statement, Object parameter);
+	int delete(String statement, Object parameter);
+	
+	<T> T selectOne(String statement);
+	<E> List<E> selectList(String statement);
+	<K,V> Map<K,V> selectMap(String statement, String mapKey);
+	int insert(String statement);
+	int update(String statement);
+	int delete(String statement);
 	
 }
